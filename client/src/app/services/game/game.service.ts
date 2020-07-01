5ee5dd2d2a1fe4111054f933524c70e52c43cfee
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2020-07-02 00:45:18 +0100

    Add partially completed GameService to track and modify game state in client

--- client/src/app/services/game/game.service.ts
+++ client/src/app/services/game/game.service.ts
@@ -1,12 +1,56 @@
 import { Injectable } from '@angular/core';
 import { SignalrService } from '../signalr/signalr.service';
 import { GameClient } from '../game-api.service';
+import { GameBoard } from 'src/app/server-models/gameBoard';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  constructor(private api: GameClient, private signalr: SignalrService) { }
+  public inGame = false;
+  // Means that game is in play, service may still be connected to a
+  // game that hasn't started yet or that has finished if false.
 
-  
+  public playerNumber: number;
+  public myTurn: boolean;
+  public victory: boolean;
+  public game = new BehaviorSubject<GameBoard>(null);
+  public connectionError: Subject<string>;
+  public invalidMoveError: Subject<void>;
+
+  private gameId: string;
+
+  constructor(private api: GameClient, private signalr: SignalrService) {
+    this.registerErrorMessages();
+    this.registerGameEvents();
+  }
+
+  createGame() {
+    this.api.startGame().subscribe(gameId => {
+      this.gameId = gameId;
+      this.signalr.connectToGame(gameId);
+    });
+  }
+
+  makeMove() {
+
+  }
+
+  leaveGame() {
+    throw new Error('Not implemented.');
+  }
+
+  private registerErrorMessages() {
+    this.signalr.gameNotFound.subscribe(() => this.connectionError.next('Could not connect to the specified game (not found).'));
+    this.signalr.gameFull.subscribe(() => this.connectionError.next('Could not connect to the specified game (already full).'));
+    this.invalidMoveError = this.signalr.invalidMove; // Pass through.
+  }
+
+  private registerGameEvents() {
+    this.signalr.addedToGame.subscribe(playerNumber => this.playerNumber = playerNumber);
+    this.signalr.gameStart.subscribe(() => this.inGame = true);
+    this.signalr.nextToMove.subscribe(nextNumber => this.myTurn = nextNumber == this.playerNumber);
+    this.signalr.winningPlayer.subscribe(winnerNumber => this.victory = winnerNumber == this.playerNumber);
+  }
 }
